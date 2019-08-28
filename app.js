7f101de07fb1c8db0e95a74d209f5d8530a1fbae
Jonathan Glassey <jonofilms@gmail.com>
2019-08-28 23:25:56 +0900

    Add dynamic screen fill on load, add numCols, extraColors params

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-const COLORS = [
-  'red', 
-  'purple', 
-  'blue', 
-  'green',
-  'pink',
-  'yellow',
-  'orange',
-  'grey'
+const DEFAULT_COLORS = [
+  '#F37A8E', 
+  '#CADE81', 
+  '#B7ACCD', 
+  '#FCB768',
+  '#84BFD1',
 ];
 
-const SQUARES_PER_LINE=5
-const ROWS=5
+url = new URL(window.location.href)
+userSetColumns = url.searchParams.get('numCol')
+userSetExtraColors = url.searchParams.get('extraColors')
+
+const COLORS = DEFAULT_COLORS.concat(userSetExtraColors.replace(/\!/g, '#').split(','))
+
+const SQUARES_PER_LINE= parseInt(userSetColumns) || 5
+const WIDTH=window.innerWidth / SQUARES_PER_LINE
+// const ROWS=5
 
 var grid = [];
 
 var appContainer = document.getElementById('app')
 
-for (var i=0; i < ROWS; i+=1) {
+for (var i=0; i*WIDTH < window.innerHeight; i+=1) {
   grid[i] = new Array(SQUARES_PER_LINE)
 }
 
@@ -39,10 +43,10 @@ for (var i=0; i < grid.length; i+=1) {
 function randomizeGrid() {
   for (var i=0; i < grid.length; i+= 1) {
     var row = document.getElementsByClassName(`row-${i}`)[0]
-    row.setAttribute('style', `height: ${100/ROWS}%`)
+    row.setAttribute('style', `height: ${WIDTH}px`)
     for(var j=0; j < grid[i].length; j+= 1) {
       block = row.getElementsByClassName(`column-${j}`)[0];      
-      block.setAttribute('style', `background\:${randomColor()}; height:100%; width:${100 / SQUARES_PER_LINE}%`)
+      block.setAttribute('style', `background\:${randomColor()}; height:100%; width:${WIDTH}px`)
     }
   }
 }
